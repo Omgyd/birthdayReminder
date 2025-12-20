f5cc1d02b697eeb0e3fe663778c430fe0be2fa5f
Benjamin Lowry <Lowry_46@hotmail.com>
2025-12-19 20:39:17 -0500

    completed sendEmail function.

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"encoding/csv"
+	"fmt"
 	"log"
+	"net/smtp"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/joho/godotenv"
@@ -56,24 +59,69 @@ func checkTodaysBirthdays(birthdays []Birthday) []Birthday {
 	return todaysBirthdays
 }
 
-func sendEmail(name []string, age int) {
+func calculateAge(birthdate time.Time) int {
+	now := time.Now()
+	age := now.Year() - birthdate.Year()
+	if now.YearDay() < birthdate.YearDay() {
+		age--
+	}
+	return age
+}
+
+func sendEmail(birthdays []Birthday) {
+	if len(birthdays) == 0 {
+		return // No birthdays today, no need to send email
+	}
+
 	password := os.Getenv("EMAIL_PASSWORD")
 	fromEmail := os.Getenv("FROM_EMAIL")
 	toEmail := []string{os.Getenv("TO_EMAIL")}
 
-	if len(name) > 1 {
-		// Figure out how to create message with multiple people
-	} else if len(name) == 1 {
-		//Set message for one person
-	}
-	// Finish logic to send email
+	var subject, body string
+	if len(birthdays) > 1 {
+		names := make([]string, len(birthdays))
+		for i, b := range birthdays {
+			names[i] = b.Name
+		}
+		nameList := strings.Join(names[:len(names)-1], ", ") + " and " + names[len(names)-1]
+		subject = fmt.Sprintf("Happy Birthday to %s!", nameList)
 
+		bodyParts := []string{}
+		for _, b := range birthdays {
+			age := calculateAge(b.Birthdate)
+			bodyParts = append(bodyParts, fmt.Sprintf("%s (%d)", b.Name, age))
+		}
+		body = fmt.Sprintf("Happy Birthday to %s! Hope you all have a wonderful day!", strings.Join(bodyParts, ", "))
+	} else {
+		b := birthdays[0]
+		age := calculateAge(b.Birthdate)
+		subject = fmt.Sprintf("Happy %dth Birthday to %s!", age, b.Name)
+		body = fmt.Sprintf("Happy Birthday to %s! Hope you have a wonderful day!", b.Name)
+	}
+
+	smtpHost := "smtp.gmail.com"
+	smtpPort := "587"
+	auth := smtp.PlainAuth("", fromEmail, password, smtpHost)
+
+	msg := []byte(fmt.Sprintf("To: %s\r\nSubject: %s\r\n\r\n%s", strings.Join(toEmail, ","), subject, body))
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, fromEmail, toEmail, msg)
+	if err != nil {
+		log.Fatal("Failed to send email:", err)
+	}
+	log.Println("Email sent successfully!")
 }
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
+	}
+	birthdays := readCSVFile("./data/birthdays.csv")
+	todaysBirthdays := checkTodaysBirthdays(birthdays)
+	if len(todaysBirthdays) > 0 {
+		sendEmail(todaysBirthdays)
+	} else {
+		log.Println("No birthdays today")
 	}
 
 }
